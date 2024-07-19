b1b782d11b6049c4525959bd15fdf9d32dc17ff3
Muhammad Amirul <mirulid@gmail.com>
2024-07-19 09:01:43 +0700

    add dto

---
+++ src/main/java/thefantasticfour/myExpertTranslator/util/dto/TranslateDto.java
@@ -0,0 +1,13 @@
+package thefantasticfour.myExpertTranslator.util.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TranslateDto {
+    private String Content;
+}
