287f4363f647730e84d2cd077dc4a5e0bd1b9808
fauzangan <fauzan12356@gmail.com>
2024-07-19 10:00:38 +0700

    test

---
+++ src/main/java/thefantasticfour/myExpertTranslator/service/TranslationService.java
@@ -0,0 +1,7 @@
+package thefantasticfour.myExpertTranslator.service;
+
+import thefantasticfour.myExpertTranslator.util.dto.TranslateDto;
+
+public interface TranslationService {
+    public TranslateDto translate(TranslateDto translateDto);
+}
