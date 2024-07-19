9706556eeb8dbecb54e860ce48709feaf9463802
Muhammad Amirul <mirulid@gmail.com>
2024-07-19 08:41:16 +0700

    first

---
+++ src/main/java/thefantasticfour/myExpertTranslator/MyExpertTranslatorApplication.java
@@ -0,0 +1,13 @@
+package thefantasticfour.myExpertTranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpertTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyExpertTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thefantasticfour/myExpertTranslator/MyExpertTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package thefantasticfour.myExpertTranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyExpertTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
